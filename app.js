c24de5dc7c74d8fccfc1b5e71aa5db14fbaabb4d
M Fathurrohman Mauludin <54023601+MFathurrohmanMauludin@users.noreply.github.com>
2022-04-30 08:32:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,12 @@
-// const {google} = require('googleapis');
-// const path = require('path');
-// const fs = require('fs');
-
-// const CLIENT_ID = '914645778845-qeslk39h7tqjg1co8hsb4ib6eja9ilp0.apps.googleusercontent.com';
-// const CLIENT_SECRET = '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';
-// const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-
-// const REFRESH_TOKEN = '1//04LRmJ0dM6MVSCgYIARAAGAQSNwF-L9Ir-jIhS3nGsDC3NT2dgqhadTgNiusxLL73Hjy-K_IsPZ0trTmkNfdFiSzSyQ7D7uao3CM';
-
-// const oauth2Client = new google.auth.OAuth2(
-//     CLIENT_ID,
-//     CLIENT_SECRET,
-//     REDIRECT_URI
-// );
-
-// oauth2Client.setCredentials({referesh_token: REFRESH_TOKEN});
-
-// const drive = google.drive({
-//     version: 'v3',
-//     auth: oauth2Client
-// });
-
-// const filePath = path.join(__dirname, 'milize.jpg');
-
-// async function uploadFile() {
-//     try {
-//         const response = await drive.files.create({
-//             requestBody: {
-//                 name: 'vladilena-milize.jpg',
-//                 mimeType: 'image/jpg'
-//             },
-//             media: {
-//                 mimeType: 'image/jpg',
-//                 body: fs.createReadStream(filePath)
-//             }
-//         });
-
-//         console.log(response.data);
-//     } catch (error) {
-//         console.log(error.message);    
-//     }
-// }
-
-// uploadFile();
-
 const { google } = require('googleapis');
 const path = require('path');
 const fs = require('fs');
 
-const CLIENT_ID = '914645778845-qeslk39h7tqjg1co8hsb4ib6eja9ilp0.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
+const CLIENT_ID = 'Masukkan client id anda';
+const CLIENT_SECRET = 'Masukkan client secret anda';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
-const REFRESH_TOKEN = '1//04puvzRPU8RAXCgYIARAAGAQSNwF-L9IrwVN7-7ta0dmQnUENayUKQWYphJCHfEwE7Wf4phBIZy9mTmhAyEhZDdIw8ixigR8zPKs';
+const REFRESH_TOKEN = 'Masukkan Refresh Token';
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -78,7 +32,7 @@ async function uploadFile() {
   try {
     const response = await drive.files.create({
       requestBody: {
-        name: 'vladilena-milize.jpg', //This can be name of your choice
+        name: 'Masukkan path/lokasi file yang akan diupload ke google drive (format foto harus disesuaikan dengan mimeType)', //This can be name of your choice
         mimeType: 'image/jpg',
       },
       media: {
@@ -98,7 +52,7 @@ async function uploadFile() {
 async function deleteFile() {
     try {
         const response = await drive.files.delete({
-            fileId: '14ou3pUqZoPOWav7REsm5hzi9PwRgqs4N',
+            fileId: 'Masukkan id file dari file yang sudah diupload',
         });
         console.log(response.data, response.status);
     } catch (error) {
@@ -110,7 +64,7 @@ async function deleteFile() {
 
 async function generatePublicUrl() {
   try {
-    const fileId = '12zepA2nY56JV3aaNVw4S-3L-Jp38IP2W';
+    const fileId = 'Masukkan id file dari file yang sudah diupload';
     await drive.permissions.create({
       fileId: fileId,
       requestBody: {
